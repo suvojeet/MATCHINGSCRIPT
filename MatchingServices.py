d8a4a7c7f59113a9ef17d3fbd2d3b8bd85061b8e
Suvojeet Pal <82687701+suvojeet@users.noreply.github.com>
2021-06-07 17:19:08 -0500

    update matching services

--- MatchingServices.py
+++ MatchingServices.py
@@ -14,16 +14,16 @@ class MatchingEngine(Resource):
             req_json = request.get_json(cache=True)
             #matchparam=''
             #dictPosition = 1
-            print(req_json)
+            print("Hello")
             print(req_json['firstname'])
             #print(matchparam)
             engineInstance=MatchProcessing()
             suspectList=engineInstance._compareMatching_(req_json['firstname'],req_json['lastName'],req_json['suffix'],
-                                                         req_json['gender'],req_json['ssn'],req_json['dob'],req_json['address'],req_json['inputparam1'],req_json['inputparam2'])
+                                                         req_json['gender'],req_json['custExternalId'],req_json['dob'],req_json['address'],req_json['inputparam1'],req_json['inputparam2'])
                 
             jsonStr = json.dumps(suspectList)
            
-            return {'Suspect': jsonStr}, 201
+            return {'Suspect': jsonStr}, 200
 
         
 api.add_resource(MatchingEngine,'/matchingengine',methods=['POST'])
